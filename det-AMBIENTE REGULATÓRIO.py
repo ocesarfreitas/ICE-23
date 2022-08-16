9bb037e40c85aba6feb9dddac49747a128776f00
vitorbborges <vitorbborges@terra.com.br>
2022-08-16 15:16:56 -0300

    mercado

--- det-AMBIENTE REGULATÓRIO.py
+++ det-AMBIENTE REGULATÓRIO.py
@@ -1,7 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 15 19:43:57 2022
+import pandas as pd
+import numpy as np
+from funcs import *
 
-@author: vitor
-"""
+# 2.2. DETERMINANTE AMBIENTE REGULATÓRIO
 
+# ---------------------------------------------------------------------------------------------
+# 2.2.1. SUBDETERMINANTE TEMPO DE PROCESSOS
+subdet = 'Tempo de processos'
+
+def read_excel(filenames):
+    return pd.read_excel(filenames, header = 1, usecols = "I,R,AA")
+    
+if __name__ == "__main__":
+    files = glob.glob("DETERMINANTE AMBIENTE REGULATÓRIO/REDESIM/*.xlsx")
+
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+        data = pool.map(read_excel, files)
+        sub_tempo_pro = pd.concat(data)
+
+# ---------------------------------------------------------------------------------------------
+# 2.2.2. SUBDETERMINANTE TRIBUTAÇÃO
