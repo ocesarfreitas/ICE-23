2a791309df12ff24dc334576a0fff49a65f9ecd4
ocesarfreitas <cesarfreitasalbuquerque@gmail.com>
2022-09-01 10:47:46 -0300

    Create det-CAPITAL-HUMANO.py

---
+++ det-CAPITAL-HUMANO.py
@@ -0,0 +1,24 @@
+"""
+Created on Thu Aug 18 16:50:42 2022
+
+@author: CLIENTE
+"""
+
+import pandas as pd
+#import numpy as np
+from funcs import *
+#from functools import reduce
+import basedosdados as bd
+
+# 2.2. DETERMINANTE AMBIENTE REGULATÓRIO
+database = pd.DataFrame()
+df = {}
+
+# 1. AMOSTRA
+
+amostra = pd.read_csv('AMOSTRA/100-municipios.csv', converters={i: str for i in range(0,101)})
+amostra['Cod.IBGE'] = amostra['COD. UF'] + amostra['COD. MUNIC']
+database['Município'] = amostra['NOME DO MUNICÍPIO'].str.upper().str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+database['UF'] = amostra['UF']
+database['Cod.IBGE'] = amostra['Cod.IBGE']
+database = database.set_index(['Município', 'UF'])
