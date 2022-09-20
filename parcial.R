00cf0c4f011236dd7ca0bc449e9f56d0cbf1ae47
ocesarfreitas <cesarfreitasalbuquerque@gmail.com>
2022-09-20 17:51:19 -0300

    update

--- parcial.R
+++ parcial.R
@@ -3,9 +3,9 @@ library("tidyverse")
 
 #-------------------------------------------------------------------------------
 # Criando looping para importar os indicadores padronizados
-det <- c("ACESSO A CAPITAL","AMBIENTE REGULATÓRIO","CAPITAL HUMANO","CULTURA",
+det <- c("ACESSO A CAPITAL","AMBIENTE REGULATÓRIO","CAPITAL HUMANO","CULTURA2",
          "INFRAESTRUTURA","INOVACAO","MERCADO")
-det_s <- c("ACESSO_CAPITAL","AMBIENTE_REGULATORIO","CAPITAL_HUMANO","CULTURA",
+det_s <- c("ACESSO_CAPITAL","AMBIENTE_REGULATORIO","CAPITAL_HUMANO","CULTURA2",
            "INFRAESTRUTURA","INOVACAO","MERCADO")
 
 arq <- list()
@@ -23,4 +23,4 @@ determinantes <- df %>%
   reduce(full_join, by=c('V1','V2'))
 
 colnames(determinantes)[1:2] <- c("Município","UF")
-write.csv(determinantes, 'DETERMINANTES/parcial.csv')
+write.csv(determinantes, 'DETERMINANTES/parcial (cultura).csv')
